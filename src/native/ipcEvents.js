1628dfd680ceab56f8dfd1193ebcf874c4781f2e
Yuri Maciel <54074819+yrimaciel@users.noreply.github.com>
2022-07-19 17:47:57 -0300

    wss fix

--- src/native/ipcEvents.js
+++ src/native/ipcEvents.js
@@ -7,13 +7,15 @@ const log = require('../logConfig')
 ipcMain.on('ws', (event) => {
   let child
 
+  let wsServer;
+
   if(isDev) {
     child = fork(path.join(__dirname, "../src/native/websocket.js"))
+    wsServer = "ws://localhost:5050"
   } else {
     child = fork(path.join(process.resourcesPath, "src/native/websocket.js"))
+    wsServer = "wss://bardemu-backend.herokuapp.com/"
   }
-
-  const wsServer = process.env.WS_SERVER
 
   log.info(`WS-SERVER: ${wsServer}`)
 
