9cc942985c2fc3dc598217de66d66b253f73364f
Yuri Maciel <54074819+yrimaciel@users.noreply.github.com>
2022-08-09 19:46:53 -0300

    v1.1.1

--- vue.config.js
+++ vue.config.js
@@ -36,7 +36,7 @@ module.exports = {
           // private: true,
           owner: "yrimaciel",
           repo: "bardemu-desktop",
-          token: "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"
+          token: "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"
         },
         win: {
           icon: './build/app-icon.png'
